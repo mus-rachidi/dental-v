e4b306cddc901bc4081f665c3ea764f261774740
Mustapha Rachidi <mustapha.rachidi@oracle.com>
2026-03-07 12:21:55 +0000

    ui win32

--- ClinicManager/obj/Debug/net8.0-windows/fr/ClinicManager.resources.cs
+++ ClinicManager/obj/Debug/net8.0-windows/fr/ClinicManager.resources.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © 2026 ClinicSoft. All rights reserved.")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Professional Clinic Management System")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4c3b9213ffe61a8d21aef8b7750c894a2d26c7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9591e0a4a96790056aa21cab4088b265993cf45d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Clinic Management System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
